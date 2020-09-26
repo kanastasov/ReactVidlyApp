53b65cc63bda6e95fb81bf7c0b194488a60a38cb
kanastasoff <kireprogramista@gmail.com>
2020-09-26 07:26:22 +0300

    destructured the components

--- src/components/common/table.jsx
+++ src/components/common/table.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import TableBody from './tableBody';
 import TableHeader from './tableHeader';
 
-const Table = (props) => {
-    const { columns, sortColumn, onSort, data } = props;
+const Table = ({ columns, sortColumn, onSort, data }) => {
     return ( 
         <table className="table">
         <TableHeader 
