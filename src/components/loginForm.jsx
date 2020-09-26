f7c90d16898ba33592e79972f220e47e389516e8
kanastasoff <kireprogramista@gmail.com>
2020-09-26 11:14:28 +0300

    controlled elements

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 
 class LoginForm extends Component {
+
+    state= {
+        account: {username: '', password: ''}
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+        console.log('Submited');
+    }
+    handleChange = (e) => {
+        const account = {...this.state.account};
+        account.username = e.currentTarget.value;
+        this.setState({ account });
+    };
     render() { 
         return ( 
         <div>
             <h1>LoginForm</h1>
-            <form >
+            <form onSubmit={this.handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="username"></label>
-                    <input id="username" type="text" className="form-control"/>Username</div>
+                    <input 
+                      value={this.state.account.username}
+                      onChange={this.handleChange}
+                      id="username" type="text"
+                      className="form-control"/>
+                      Username
+                      </div>
                 <div className="form-group">
                     <label htmlFor="password"></label>
                     <input id="password" type="text" className="form-control"/>Password
