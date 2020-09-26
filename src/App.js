ab438483c04b4359c4db2b24e28c955188817821
kanastasoff <kireprogramista@gmail.com>
2020-09-26 09:35:48 +0300

    linking the movie form

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import React, { Component } from 'react';
-import { Route, Redirect, Switch } from 'react-router-dom';
-import './App.css';
-import Movies from './components/movies';
-import Customers from './components/customers';
-import Rentals from './components/rentals';
-import NotFound from './components/notFound';
-import NavBar from './components/navBar';
-
+import React, { Component } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
+import Movies from "./components/movies";
+import MovieForm from "./components/movieForm";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
+import "./App.css";
 
 class App extends Component {
-  render() { 
+  render() {
     return (
       <React.Fragment>
-      <NavBar />
-      <main className="container" >
-        <Switch>
-          <Route path="/movies" componet={Movies}></Route>
-          <Route path="/customers" componet={Customers}></Route>
-          <Route path="/rentals" componet={Rentals}></Route>
-          <Route path="/not-found" componet={NotFound}></Route>   
-          <Redirect from="/" exact to="/movies" />
-          <Redirect to="not-found" />
-        </Switch>
-    
-      </main>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
       </React.Fragment>
     );
-  } 
+  }
 }
- 
+
 export default App;
