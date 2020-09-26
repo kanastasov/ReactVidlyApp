12d093659f88b6c719ecfed3e2239a4bb0b7b432
kanastasoff <kireprogramista@gmail.com>
2020-09-26 12:34:05 +0300

    getting started with Joi library for validation

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Input from './common/input';
+import Joi from 'joi-browser';
 
 class LoginForm extends Component {
 
@@ -7,8 +8,17 @@ class LoginForm extends Component {
         account: {username: "", password: ""},
         errors: {}
     };
+    schema = {
+        username: Joi.string().required(),
+        password: Joi.string().required()
+    };
 
     validate = () => {
+
+        const result = Joi.validate(this.state.account,this.schema,
+            {abortEarly:false});
+        console.log(result);
+
         const errors = {};
         const { account } = this.state;
         if(account.username.trim() ==='')
