053d6f0af0766b3c0765078733ed55a9a9a1b912
kanastasoff <kireprogramista@gmail.com>
2020-09-26 09:23:24 +0300

    Added Boostrap navbar

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import Movies from './components/movies';
 import Customers from './components/customers';
 import Rentals from './components/rentals';
 import NotFound from './components/notFound';
+import NavBar from './components/navBar';
 
 
 class App extends Component {
   render() { 
     return (
+      <React.Fragment>
+      <NavBar />
       <main className="container" >
         <Switch>
           <Route path="/movies" componet={Movies}></Route>
@@ -19,7 +22,9 @@ class App extends Component {
           <Redirect from="/" exact to="/movies" />
           <Redirect to="not-found" />
         </Switch>
+    
       </main>
+      </React.Fragment>
     );
   } 
 }
