5297382d0b1e2385dc68233e8b3fa9bf9a29d7b9
kanastasoff <kireprogramista@gmail.com>
2020-09-27 09:30:24 +0300

    refactor the services with global config

--- src/services/genreService.js
+++ src/services/genreService.js
@@ -1,7 +1,8 @@
 import http from './httpService';
+import {apiUrl} from '../config.json';
 
 
 export function getGenres() {
-    return http.get('http://localhost:3900/api/genres');
+    return http.get(apiUrl + "/genres");
   }
   
