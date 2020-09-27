77a9bf342f7ec1ed0fda22ab0d985aaacfe4a885
kanastasoff <kireprogramista@gmail.com>
2020-09-27 10:45:52 +0300

    handling login errors

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Joi from 'joi-browser';
 import Form from './common/form';
+import { login } from '../services/authService';
 
 class LoginForm extends Form {
 
@@ -13,7 +14,18 @@ class LoginForm extends Form {
         password: Joi.string().required().label('Password')
     };
 
-    doSubmot = () =>{
+    doSubmit = async () =>{
+        try {
+            const { data } = this.state;
+            await login(data.username, data.password);      
+        } catch (ex) {
+           if(ex.response && ex.response.status ===400) {
+               const errors = {... this.state.errors};
+               errors.username=ex.response.data;
+               this.setState=({errors});
+           } 
+        }
+      
         console.log('Submited');
     };
 
