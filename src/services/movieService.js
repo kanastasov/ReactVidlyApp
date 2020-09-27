f97f3bebbe74395935ce8e836609253f1c678c4c
kanastasoff <kireprogramista@gmail.com>
2020-09-27 09:39:21 +0300

    refactored the movie form

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -6,6 +6,14 @@ export function getMovies() {
     return http.get(apiEndpoint);
 }
 
+export function getMovie(moviedId) {
+    return http.get(apiEndpoint + '/' + moviedId);
+}
+
+export function saveMovie(movie) {
+
+}
+
 export function deleteMovie(movieId) {
     return http.delete(apiEndpoint+ '/' + movieId);
 }
