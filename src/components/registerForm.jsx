bd1a7b561b5345b474427aaa1dd3e18d83a12a79
kanastasoff <kireprogramista@gmail.com>
2020-09-27 11:22:53 +0300

    x-auth-token

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -26,7 +26,10 @@ class RegisterForm extends Form {
   doSubmit = async () => {
     // Call the server
     try {
-      await userService.register(this.state.data);
+      const response = await userService.register(this.state.data);
+      localStorage.setItem("token", response.headers["x-auth-token"]);
+      this.props.history.push("/");
+      console.log(response);
     } catch (ex) {
         if(ex.response && ex.response.status === 400) {
             const errors = {...this.state.errors};
