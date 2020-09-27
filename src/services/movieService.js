f65803f1e1733449ea0bb16836c17577825f47b1
kanastasoff <kireprogramista@gmail.com>
2020-09-27 09:42:53 +0300

    fix the update and save functions

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -11,7 +11,12 @@ export function getMovie(moviedId) {
 }
 
 export function saveMovie(movie) {
-
+    if(movie._id) { 
+        const body = {...movie};
+        delete body._id;
+        return http.put(apiEndpoint + '/' + movie._id, body);
+    }
+    return http.post(apiEndpoint, movie);
 }
 
 export function deleteMovie(movieId) {
